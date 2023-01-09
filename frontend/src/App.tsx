5cb6cf70586af32d4d1652133f637c01e3d1ae42
이동건 <developer@idong-geon-ui-MacBookPro.local>
2023-01-09 11:51:15 +0900

    map 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      {catItems?.map((catItem) => (
+      {/* {catItems?.map((catItem) => (
         <div key={catItem._id}>
           <img src={catItem.image} />
           <h2>{catItem.breeds}</h2>
@@ -34,7 +34,8 @@ const App: React.FC = () => {
           <h2>{catItem.name}</h2>
           <p>{catItem.description}</p>
         </div>
-      )) || []}
+      )) || []} */}
+      <h1>Hello World 목표는 데이터 받아오기</h1>
     </div>
   );
 };
