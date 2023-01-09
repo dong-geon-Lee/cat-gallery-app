1357495928bbd0a8436689d37075f745ba2967bc
이동건 <developer@idong-geon-ui-MacBookPro.local>
2023-01-09 09:44:03 +0900

    코드 수정 db

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const connectedDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGO_URL, () => {
-      console.log("MongoDB Connected!");
-    });
+    await mongoose
+      .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("Connected MongoDB!"));
   } catch (error) {
     console.log(error.message);
     process.exit(1);
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
 const cors = require("cors");
 const connectedDB = require("./config/database");
 const cats = require("./routes/cats");
+
+dotenv.config();
+connectedDB();
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -11,5 +14,4 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/cats", cats);
 
-connectedDB();
 app.listen(port, () => console.log(`http://localhost:${port}`));
