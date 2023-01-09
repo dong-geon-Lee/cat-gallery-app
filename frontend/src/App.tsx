765089011d71b78727fc38e6ddac5114f4680eb9
이동건 <developer@idong-geon-ui-MacBookPro.local>
2023-01-09 12:26:57 +0900

    map 로직 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,16 +26,16 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      {catItems?.map((catItem) => (
-        <div key={catItem._id}>
-          <img src={catItem.image} />
-          <h2>{catItem.breeds}</h2>
-          <h2>{catItem.age}</h2>
-          <h2>{catItem.name}</h2>
-          <p>{catItem.description}</p>
-        </div>
-      )) || []}
-      <h1>Hello World 목표는 데이터 받아오기</h1>
+      {catItems &&
+        catItems?.map((catItem) => (
+          <div key={catItem._id}>
+            <img src={catItem.image} />
+            <h2>{catItem.breeds}</h2>
+            <h2>{catItem.age}</h2>
+            <h2>{catItem.name}</h2>
+            <p>{catItem.description}</p>
+          </div>
+        ))}
     </div>
   );
 };
