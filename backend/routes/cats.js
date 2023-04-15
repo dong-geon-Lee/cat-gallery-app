c3bb03870e8c3a1314219cc4f59df71df4d2a278
dong-geon-Lee <ch9716@naver.com>
2023-04-15 16:10:36 +0900

    코드 정리 및 로고 추가

--- backend/routes/cats.js
+++ backend/routes/cats.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { getCats, addCats } = require("../controllers/cats");
 const upload = require("../middleware/multerUpload");
+const { getCats, addCats } = require("../controllers/cats");
 
 router.get("/", getCats);
 router.post("/", upload.single("image"), addCats);
